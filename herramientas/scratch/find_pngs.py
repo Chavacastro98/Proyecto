bcf9721fff8439784b46ebb8b691487350522240
Chavacastro98 <salvadorcastroperez@hotmail.com>
2026-09-16 16:24:19 -0600

    chore: remover documentos academicos, scratch, telemetria experimental y duplicados de git

--- herramientas/scratch/find_pngs.py
+++
@@ -1,7 +0,0 @@
-import re
-
-with open(r'c:\Proyecto\Proyecto\documentos\MANUAL_DE_OPERACION_QUIMICA.html', encoding='utf-8') as f:
-    for i, line in enumerate(f):
-        m = re.findall(r'src=["\']([^"\']+\.png)["\']', line)
-        if m:
-            print(f'{i+1}: {m}')
